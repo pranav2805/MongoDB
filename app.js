d64f454fbeac5de63bb7315bd26676c88de67c01
pranav2805 <pranavpradeep095@gmail.com>
2023-07-18 18:20:41 +0530

    Intro to mongoose & created Product model

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const mongoConnect = require('./util/database').mongoConnect;
+const mongoose = require('mongoose');
+
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,20 +20,25 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('64b5555a08568c0af1245ae8')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('64b5555a08568c0af1245ae8')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose.connect('mongodb+srv://pranavpradeep095:Vsr2PfxTCUsNKGTN@cluster0.uqrnxyd.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(result => {
+    console.log('Connected!');
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  })
